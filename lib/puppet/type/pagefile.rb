ptierno/puppet-windowspagefile

commit e9b4208fa7dabc47dc9334e6a6a6c4b7ded21aa3

Conditional munge based on $operatingsystemrelease
https://github.com/ptierno/puppet-windowspagefile/issues/2

In Windows Server 2008, wmi was returning pagefile paths with an
uppercase drive letter, where as in 2012 it was returning a lowercase
drive letter. Wrapped the munge for the namevar in an if statement to
check the $::operatingsystemrelease fact and munge appropriately.

lib/puppet/type/pagefile.rb
@@ -1,3 +1,5 @@
+require 'puppet/property/boolean'
+
 Puppet::Type.newtype(:pagefile) do
   ensurable
 
@@ -7,14 +9,19 @@ Puppet::Type.newtype(:pagefile) do
     validate do |value|
       fail("Invalid page file name. Must be an absolute path, got #{value}") unless Pathname.new(value).absolute?
     end
+
     munge do |value|
-      value.capitalize
+      if Facter['operatingsystemrelease'] =~ /2012/
+        value.downcase
+      else
+        value.capitalize
+      end
     end
   end
 
-  newproperty(:systemmanaged) do
+  newproperty(:systemmanaged, :boolean => true, :parent => Puppet::Property::Boolean) do
     desc 'Whether or not the pagefile size is automatically managed by the system'
-    newvalues(:true,:false)
+    #newvalues(:true,:false)
   end
 
   newproperty(:initialsize) do

lib/puppet/provider/pagefile/wmi.rb
@@ -34,9 +34,9 @@ Puppet::Type.type(:pagefile).provide(:wmi) do
       # if the initial and maximum size of the pagefile == 0
       # then the size is managed by the operating system
       if pagefile.InitialSize == 0 and pagefile.MaximumSize == 0
-        system_managed = :true
+        system_managed = true
       else
-        system_managed = :false
+        system_managed = false
       end
       new(
         :name          => pagefile.Name,
@@ -93,8 +93,8 @@ Puppet::Type.type(:pagefile).provide(:wmi) do
   end
 
   def systemmanaged=(value)
-    @property_flush[:initialsize]   = 0
-    @property_flush[:maximumsize]   = 0
+    @property_flush[:initialsize]   = 0 if value
+    @property_flush[:maximumsize]   = 0 if value
     @property_flush[:systemmanaged] = value
   end
 
@@ -113,10 +113,9 @@ Puppet::Type.type(:pagefile).provide(:wmi) do
 
       validate_props
 
-      instance = adsi.wmi_connection.InstancesOf('Win32_PageFileSetting').to_enum.find { |f| f.Name == resource[:path] }
-      pagefile = instance if @property_flush[:initialsize] or @property_flush[:maximumsize] or @property_flush[:systemmanaged]
+      pagefile = adsi.wmi_connection.InstancesOf('Win32_PageFileSetting').to_enum.find { |f| f.Name == resource[:path] }
 
-      if pagefile
+      if @property_flush[:initialsize] or @property_flush[:maximumsize] or @property_flush[:systemmanaged]
         pagefile.InitialSize = @property_flush[:initialsize]
         pagefile.MaximumSize = @property_flush[:maximumsize]
         pagefile.Put_()
@@ -152,7 +151,7 @@ Puppet::Type.type(:pagefile).provide(:wmi) do
   def validate_props
     # cannot set initial or maximumsize if
     # the system is managing the pagefile size
-    if (resource[:initialsize] or resource[:maximumsize]) and resource[:systemmanaged] == :true
+    if (resource[:initialsize] or resource[:maximumsize]) and resource[:systemmanaged]
       resource.fail('initialsize and maximumsize should not be used with systemmanaged.')
     end
   end

commit d5a6c52d60cb7c6bdd6d1bd77710cbc2f1774147

Fix issue with munge
Fixes issue #3

The commit e9b4208 introduced an issue where the
pagefile name was not munging correctly. This was due
to not retrieving the Facter value for operatingsystemrelease
properly.

lib/puppet/type/pagefile.rb
@@ -11,7 +11,7 @@ Puppet::Type.newtype(:pagefile) do
     end
 
     munge do |value|
-      if Facter['operatingsystemrelease'] =~ /2012/
+      if Facter.value(:operatingsystemrelease) =~ /2012/
         value.downcase
       else
         value.capitalize